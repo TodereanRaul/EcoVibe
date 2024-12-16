c7d988b066a5cef2d60ed3d5a0e9caba5b478a0c
Toderean R. <raultoderean20@gmail.com>
2024-12-16 12:20:44 +0100

    Add Forum Design

--- Frontend/src/app/components/forum/components/ecovibe-post-content/ecovibe-post-content.component.ts
+++ Frontend/src/app/components/forum/components/ecovibe-post-content/ecovibe-post-content.component.ts
@@ -6,8 +6,16 @@ import { CommonModule } from '@angular/common';
   selector: 'ecovibe-post-content',
   imports: [CommonModule],
   templateUrl: './ecovibe-post-content.component.html',
-  styleUrl: './ecovibe-post-content.component.scss'
+  styleUrl: './ecovibe-post-content.component.scss',
 })
 export class EcovibePostContentComponent {
   @Input() post: Post | null = null;
+
+  // Create a getter to split content into paragraphs dynamically
+  get paragraphs(): string[] {
+    if (this.post && this.post.content) {
+      return this.post.content.split('\n').filter((p) => p.trim() !== '');
+    }
+    return [];
+  }
 }
